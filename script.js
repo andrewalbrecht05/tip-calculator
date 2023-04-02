aa9a8b62366466c072752cfce4cc65d56bf6d7f5
andrewalbrecht05 <andrijalbrext@gmail.com>
2023-04-02 11:38:48 +0300

    added check for incorrect input

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ function calculateTip()
     const billAmount = document.getElementById("billamount").value
     const serviceQuality = document.getElementById("servicequality").value
     const peopleCount = document.getElementById("peoplecount").value
+    console.log(peopleCount)
+    if(billAmount === '' || serviceQuality == 0 || peopleCount === '' )
+    {
+        alert("Будь ласка введіть значення");
+        return;
+    }
 
+    if( billAmount <= 0 || peopleCount <= 0 )
+    {
+        alert("Ви ввели некоректні значення");
+    }
     let total = (billAmount * serviceQuality) / peopleCount
     total = Math.round(total * 100) / 100;
     total = total.toFixed(2)
@@ -14,6 +24,7 @@ function calculateTip()
 }
 
 console.log("JS file successfully connected")
+document.getElementById("totalTip").style.display = "none"
 
 document.getElementById("calculate").onclick = function()
 {
