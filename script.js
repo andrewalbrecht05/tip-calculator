e240cf4add2b4297b958ca595b37e29dc0020433
andrewalbrecht05 <andrijalbrext@gmail.com>
2023-04-03 11:18:26 +0300

    added addEventListener

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 // Рахуємо чайові
-function calculateTip()
-{
-    const billAmount = document.getElementById("billamount").value
-    const serviceQuality = document.getElementById("servicequality").value
-    const peopleCount = document.getElementById("peoplecount").value
-    console.log(peopleCount)
-    if(billAmount === '' || serviceQuality == 0 || peopleCount === '' )
-    {
-        alert("Будь ласка введіть значення");
-        return;
+document.addEventListener("DOMContentLoaded", () => {
+    function calculateTip() {
+        const billAmount = document.getElementById("billamount").value
+        const serviceQuality = document.getElementById("servicequality").value
+        const peopleCount = document.getElementById("peoplecount").value
+        //console.log(peopleCount)
+        if (billAmount === '' || serviceQuality == 0 || peopleCount === '') {
+            alert("Будь ласка введіть значення");
+            return;
+        }
+
+        if (billAmount <= 0 || peopleCount <= 0) {
+            alert("Ви ввели некоректні значення");
+        }
+        let total = (billAmount * serviceQuality) / peopleCount
+        total = Math.round(total * 100) / 100;
+        total = total.toFixed(2)
+
+        document.getElementById("totalTip").style.display = "block"
+        document.getElementById("tip").innerHTML = total;
     }
 
-    if( billAmount <= 0 || peopleCount <= 0 )
-    {
-        alert("Ви ввели некоректні значення");
+    console.log("JS file successfully connected")
+    document.getElementById("totalTip").style.display = "none"
+
+    document.getElementById("calculate").onclick = () => {
+        calculateTip();
+        console.log("Calculation completed")
     }
-    let total = (billAmount * serviceQuality) / peopleCount
-    total = Math.round(total * 100) / 100;
-    total = total.toFixed(2)
-
-    document.getElementById("totalTip").style.display = "block"
-    document.getElementById("tip").innerHTML = total;
-}
-
-console.log("JS file successfully connected")
-document.getElementById("totalTip").style.display = "none"
-
-document.getElementById("calculate").onclick = function()
-{
-    calculateTip();
-    console.log("Calculation completed")
-}
+})
